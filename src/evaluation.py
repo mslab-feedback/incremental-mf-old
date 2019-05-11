388e86ccf42f3c92d52de6cce6ca0795e5f015cf
Huaiyu <huaiyulin1026@gmail.com>
2019-05-11 22:32:37 +0800

    update evaluation function

--- src/evaluation.py
+++ src/evaluation.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from metrics import *
 
-def evaluate(predict_path, answer_path, average=False, with_header=False):
+def evaluate(predict_path, answer_path, average=False, is_target_with_header=False, is_ranked_list_with_header=False, ranked_list_cols=None):
     """
     Parameters:
     ----------
@@ -32,13 +32,22 @@ def evaluate(predict_path, answer_path, average=False, with_header=False):
     df_ans = pd.read_csv(answer_path, header=None)
 
     # drop [user_id, event_time]
-    if with_header:
-        targets = df_ans.loc[1:,1].values.astype(int) # drop header and choose the column of targets
-        ranked_lists = df_pre.loc[:,:].values.astype(int) # drop header and choose columns of ranked items
+    if is_target_with_header:
+        targets = df_ans.loc[1:,1] # drop header and choose the column of targets
     else:
-        targets = df_ans.loc[:,1].values.astype(int) # drop header and choose the column of targets
-        ranked_lists = df_pre.loc[:,:].values.astype(int) # drop header and choose columns of ranked items
+        targets = df_ans.loc[:,1] # drop header and choose the column of targets
+    
+    if is_ranked_list_with_header:
+        ranked_lists = df_pre.loc[1,:] # drop header
+    else:
+        ranked_lists = df_pre.loc[:,:] # drop header
 
+    if ranked_list_cols:
+        ranked_lists = ranked_lists.loc[:,ranked_list_cols]  # choose columns of ranked items
+
+    targets = targets.values.astype(int) # convert dataFrame to list
+    ranked_lists = ranked_lists.values.astype(int)
+    
     # get scores of list/average
     hr = get_hit_ratio(ranked_lists,targets,average)
     mrr = get_MRR(ranked_lists,targets,average)
