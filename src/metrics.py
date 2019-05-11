12184f2402b6ff72b548c8d4b04314a6d7e3f936
Huaiyu <huaiyulin1026@gmail.com>
2019-05-11 21:49:30 +0800

    update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -10,11 +10,11 @@ def get_NDCG_(ranked_list, target):
     return 0 if len(where) == 0 else math.log(2) / math.log(where[0] + 2)
 def get_MRR_(ranked_list,target):
     try:
-        return 1./(ranked_list.index(target) + 1)
+        return 1./(list(ranked_list).index(target) + 1)
     except:
         return 0
 
-def get_hit_ratio(ranked_lists, targets):
+def get_hit_ratio(ranked_lists, targets, average=False):
     """
     Parameters
     ----------
@@ -24,14 +24,21 @@ def get_hit_ratio(ranked_lists, targets):
         ranked_lists[i,j] is the top j-th item recommended to i-th person
     targets: np.array of shape (n,)
         targets[i] is a i-th person's known positive
+    average: return a list of an average score
+        False: return a list
+        True: return an average score
     Returns
     -------
+    list[hit-ratio score]
     float
-        hit-ratio score
+        average of hit-ratio score
     """
-    return np.mean([get_hit_ratio_(ranked_lists[i], targets[i]) for i in range(len(targets))])
+    if average:
+        return np.mean([get_hit_ratio_(ranked_lists[i], targets[i]) for i in range(len(targets))])
+    else:
+        return [get_hit_ratio_(ranked_lists[i], targets[i]) for i in range(len(targets))]
 
-def get_NDCG(ranked_lists, targets):
+def get_NDCG(ranked_lists, targets, average=False):
     """
     Parameters
     ----------
@@ -41,14 +48,21 @@ def get_NDCG(ranked_lists, targets):
         ranked_lists[i,j] is the top j-th item recommended to i-th person
     targets: np.array of shape (n,)
         targets[i] is a i-th person's known positive
+    average: return a list of an average score
+        False: return a list
+        True: return an average score
     Returns
     -------
+    list[NDCG score]
     float
-        NDCG score
+        average of NDCG score
     """
-    return np.mean([get_NDCG_(ranked_lists[i], targets[i]) for i in range(len(targets))])
+    if average:
+        return np.mean([get_NDCG_(ranked_lists[i], targets[i]) for i in range(len(targets))])
+    else:
+        return [get_NDCG_(ranked_lists[i], targets[i]) for i in range(len(targets))]
 
-def get_MRR(ranked_lists, targets):
+def get_MRR(ranked_lists, targets, average=False):
     """
     Parameters
     ----------
@@ -60,7 +74,11 @@ def get_MRR(ranked_lists, targets):
         targets[i] is a i-th person's known positive
     Returns
     -------
+    list[MRR score]
     float
-        MRR score
+        average of MRR score
     """
-    return np.mean([get_MRR_(ranked_lists[i], targets[i]) for i in range(len(targets))])
+    if average:
+        return np.mean([get_MRR_(ranked_lists[i], targets[i]) for i in range(len(targets))])
+    else:
+        return [get_MRR_(ranked_lists[i], targets[i]) for i in range(len(targets))]
